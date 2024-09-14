82e4ec898da1d408414197ad8baa648e83399efe
piiyuushh <piyushkarn76@gmail.com>
2024-09-14 22:24:55 +0545

    changed animation speed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,22 @@ const Navbar = () => {
         <img src="/images/brian.png" alt="Brian" className="navbar-img" />
         <ul className="navbar-links">
           <li>
-            <Link to="hero" smooth={true} duration={200}>
+            <Link to="hero" smooth={true} duration={300}>
               Home
             </Link>
           </li>
           <li>
-            <Link to="about" smooth={true} duration={200}>
+            <Link to="about" smooth={true} duration={300}>
               About
             </Link>
           </li>
           <li>
-            <Link to="project" smooth={true} duration={200}>
+            <Link to="project" smooth={true} duration={300}>
               Projects
             </Link>
           </li>
           <li>
-            <Link to="contact" smooth={true} duration={200}>
+            <Link to="contact" smooth={true} duration={300}>
               Contact
             </Link>
           </li>
