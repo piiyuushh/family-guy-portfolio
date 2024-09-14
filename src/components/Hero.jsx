86b01a95b7257f1c315352beecad35dcfada042d
piiyuushh <piyushkarn76@gmail.com>
2024-09-14 19:22:50 +0545

    added some features

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,19 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div id="hero">
-      <section class="text-gray-600 body-font">
+      <section>
         <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+          <div>
             <img
               class="batman-img"
               alt="batman-img"
               src="/images/petman.png"
             />
           </div>
-          <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+          <div>
             <h1 class="title-font sm:text-4xl text-3xl font-medium text-gray-900 mb-3" className="about-header text-8xl">
               Piyush Karn
               <br  />
-              
             </h1>
             <h5 class="title-font sm:text-2xl text-3xl font-medium text-gray-900" className="about-header text-4xl text-black">student | web developer</h5>
           </div>
