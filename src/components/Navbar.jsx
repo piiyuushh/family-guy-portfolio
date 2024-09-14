2821c29a66c106be186c05339de4c64b59183bac
piiyuushh <piyushkarn76@gmail.com>
2024-09-14 22:24:01 +0545

    fixed contact not scrolling bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,22 @@ const Navbar = () => {
         <img src="/images/brian.png" alt="Brian" className="navbar-img" />
         <ul className="navbar-links">
           <li>
-            <Link to="hero" smooth={true} duration={500}>
+            <Link to="hero" smooth={true} duration={200}>
               Home
             </Link>
           </li>
           <li>
-            <Link to="about" smooth={true} duration={500}>
+            <Link to="about" smooth={true} duration={200}>
               About
             </Link>
           </li>
           <li>
-            <Link to="project" smooth={true} duration={500}>
+            <Link to="project" smooth={true} duration={200}>
               Projects
             </Link>
           </li>
-          <li id='contact'>
-            <Link to="contact" smooth={true} duration={500}>
+          <li>
+            <Link to="contact" smooth={true} duration={200}>
               Contact
             </Link>
           </li>
